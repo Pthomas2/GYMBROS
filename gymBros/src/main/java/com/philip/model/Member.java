869386fb73201b1b50f594d875d0a00353a99ac4
Philip Thomas <pthomas1@lions.molloy.edu>
2021-12-29 17:35:16 -0500

    inital models

---
+++ gymBros/src/main/java/com/philip/model/Member.java
@@ -0,0 +1,134 @@
+package com.philip.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "gymMembers")
+@Entity
+public class Member implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public enum Role {
+		ROLE_USER, ROLE_ADMIN
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Mbr_Id")
+	private Long mbrId;
+	
+	@Column(name = "Mbr_Fname")
+	private String fname;
+	
+	@Column(name = "Mbr_Lname")
+	private String lname;
+	
+	@Column(name = "Mbr_Email")
+	private String email;
+
+	@Column(name = "Mbr_IsAdmin", columnDefinition = "boolean default false")
+	private Boolean isAdmin;
+
+	@Column(name = "Mbr_Gym", unique = false, nullable = false)
+	private String gymName;
+
+	
+	@Column(name = "Mbr_Username", unique = true, nullable = false)
+	private String username;
+
+	@Column(name = "Mbr_Password", nullable = false)
+	private String password;
+
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Member(Long mbrId, String fname, String lname, String email, Boolean isAdmin, String gymName,
+			String username, String password) {
+		super();
+		this.mbrId = mbrId;
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.isAdmin = isAdmin;
+		this.gymName = gymName;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getMbrId() {
+		return mbrId;
+	}
+
+	public void setMbrId(Long mbrId) {
+		this.mbrId = mbrId;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public String getGymName() {
+		return gymName;
+	}
+
+	public void setGymName(String gymName) {
+		this.gymName = gymName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+
+}
---
+++ gymBros/src/test/java/com/philip/gymbros/GymBrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philip.gymbros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
